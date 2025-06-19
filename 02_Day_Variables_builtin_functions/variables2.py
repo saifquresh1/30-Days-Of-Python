2f6571f1e3676f0fb0b3c2436350bd3aa21ddc47
Saif Qureshi <142630183+saifquresh1@users.noreply.github.com>
2025-06-19 18:36:43 +0530

    update-1

---
+++ 02_Day_Variables_builtin_functions/variables2.py
@@ -0,0 +1,17 @@
+ 
+# Day 2: 30 Days of python programming
+
+first_name = "saif"
+last_name = "qureshi"
+full_name = "saif qureshi"
+country = "india"
+city = "mumbai"
+
+age = 22
+is_married = False
+is_true = "hello"
+is_light_on = True
+
+personal_info ={
+    fi
+}
